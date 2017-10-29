3f0342273a5aa3f7fb5ecdfa966fa9a7cc9a3fb7
TheVu <thevudoan@gmail.com>
2017-10-29 08:50:39 +0700

    update homework

--- fp-practices-master/Practice2/generatePractice.js
+++ fp-practices-master/Practice2/generatePractice.js
@@ -12,8 +12,28 @@ const TestType = {
   RANDOM: "RANDOM"
 };
 
+function search(input,target) {
+  return input.indexOf(target);
+}
+
+function generateArr(arr,length) {
+  const newElement = MIN_NUM + Math.floor(Math.random() * 2 * MAX_NUM);
+  const newArr = arr.concat([newElement]);
+  return length === 0 ? [] : generateArr(newArr,length-1)
+}
+
 function generate(numberOfTestcases) {
-  return Array.from({length: numberOfTestcases}); // Remove this line and use your own algorithm
+  const choice = Math.floor(Math.random() * 5);
+  const arrLength = choice === 0 ? 0 : Math.floor(Math.random() * 50);
+  const arr = generateArr([],arrLength);
+  const target = choice === 1 ? MIN_NUM + Math.floor(Math.random() * 2 * MAX_NUM) : choice === 2 ? arr[0] : choice === 3 ? arr[arr.length-1] : arr[Math.floor(Math.random()*(arr.length-1))];
+  const output = search(arr,target);
+  const obj = {
+    input : arr,
+    target : target,
+    output : output 
+  }
+  return numberOfTestcases === 0 ? [] : [obj].concat(generate(numberOfTestcases-1));
 }
 
 module.exports = generate;
